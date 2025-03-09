ff59fe6dff4ce2bf145ceebc27376337411168ac
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-09 23:23:25 +0200

    Update index.js

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,1 +1,14 @@
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+import packageReducer from './packageReducer';
+import bookingReducer from './bookingReducer';
+import serviceProviderReducer from './serviceProviderReducer';
+import alertReducer from './alertReducer';
 
+export default combineReducers({
+  auth: authReducer,
+  package: packageReducer,
+  booking: bookingReducer,
+  serviceProvider: serviceProviderReducer,
+  alert: alertReducer
+});
