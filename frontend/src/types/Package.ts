180764ed0962db1d3e3ec25d5941306e92437e3c
Victoria Fakunle <v.fakunle@alustudent.com>
2025-03-11 19:31:58 +0200

    Update Package.ts

--- frontend/src/types/Package.ts
+++ frontend/src/types/Package.ts
@@ -1,1 +1,12 @@
+export interface Package {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+  duration: number;
+  destinations: string[];
+  availability: number;
+  images: string[];
+  includedServices: string[];
+}
 
